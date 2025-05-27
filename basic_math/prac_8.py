07f90666d527dcf845830ee2b8c32656bc4a653f
ritikmishra004 <Ritikmishra1004@gmail.com>
2025-05-27 19:49:15 +0530

    Basic maths

--- basic_math/prac_8.py
+++ basic_math/prac_8.py
@@ -1,3 +1,17 @@
 # greatest common divisor
 # Highest common factor
 
+n1 = int(input ("Enter any number "))
+
+n2 = int(input("Enter any number "))
+
+while (n1>0 and n2>0):
+    if (n1>n2):
+        n1=n1%n2
+    else:
+        n2 = n2%n1
+if n1==0:
+    gcd=n2
+else:
+    gcd = n1
+print (gcd)
