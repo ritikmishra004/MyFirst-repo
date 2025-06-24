b984c40772e74984f868b43da0f709650efd4096
ritikmishra004 <Ritikmishra1004@gmail.com>
2025-06-24 21:27:17 +0530

    Bubble sort

---
+++ Sorting/prac_2.py
@@ -0,0 +1,11 @@
+# Bubble sort
+
+arr = list(map(int, input("Enter elements: ").split()))
+n = len(arr)
+
+for i in range(n):
+    for j in range(n - i - 1):
+        if arr[j] > arr[j + 1]:
+            arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+print("Sorted array:", arr)
