1adf89aa112edb2d1d426d9fa6b1cca2822181bf
ritikmishra004 <Ritikmishra1004@gmail.com>
2025-06-30 22:11:50 +0530

    list

--- List/prac_30.py
+++ List/prac_30.py
@@ -1,2 +1,42 @@
 # longest subarray with sum k
 # using hashing
+
+from typing import List
+
+def getLongestSubarray(a, k):
+    n = len(a) # size of the array.
+
+    preSumMap = {}
+    Sum = 0
+    maxLen = 0
+    for i in range(n):
+        # calculate the prefix sum till index i:
+        Sum += a[i]
+
+        # if the sum = k, update the maxLen:
+        if Sum == k:
+            maxLen = max(maxLen, i + 1)
+
+        # calculate the sum of remaining part i.e. x-k:
+        rem = Sum - k
+
+        # Calculate the length and update maxLen:
+        if rem in preSumMap:
+            length = i - preSumMap[rem]
+            maxLen = max(maxLen, length)
+
+        # Finally, update the map checking the conditions:
+        if Sum not in preSumMap:
+            preSumMap[Sum] = i
+
+    return maxLen
+
+
+a = [2, 3, 5, 1, 9]
+k = 10
+
+length = getLongestSubarray(a, k)
+print(f"The length of the longest subarray is: {length}")
+
+
+
